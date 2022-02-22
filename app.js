1c420b1ab182bad224587c3790117815a7d6c458
chengdong-xue <chengdong.xue@hp.com>
2022-02-22 10:07:45 +0800

    add mysql module
    add mysql module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const querystring = require('querystring');
 const path = require('path')
+var mysql = require("mysql");
 var bodyParser = require('body-parser');
 
 const app = express();
@@ -34,9 +35,30 @@ app.post('/saveFirebaseLogInfo', function (req, res) {
     });
     req.on('end', function() {
         console.log(JSON.stringify(req.rawBody));
+        insertEmployees(JSON.stringify(req.rawBody))
         res.status(200).json(resObj);
     });
 });
+
+const insertEmployees = (data) => {
+    const mysql = require('mysql');
+    const con = mysql.createConnection({
+        host: "15.15.161.127",
+        port: 3306,
+        user: "hpsa_tester",
+        password: "TEST@rocks_335",
+        database: "hpsa_test_data"
+    });
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+        const sql = "insert into firebase values(NULL, '32323232323', 'Iphone 13 ProMax', 'IOS', '2.2.2', '"+data+"')";
+        con.query(sql, function (err, result) {
+            if (err) console.log(sql);
+            console.log("1 record inserted");
+        });
+    });
+}
 
 app.listen(port, () => {
     console.log(`firebase_log_sys server listening on port ${port}`)
